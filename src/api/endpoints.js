dd3a365819395e32f83c52ef494e4f860fa0e7cb
MaximoGtz <maximogtzog@gmail.com>
2025-06-03 09:34:03 -0600

    antes de mover el formulario

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,6 +1,7 @@
 export function normalEndpoint(concat) {
   const localEndpoint = "http://127.0.0.1:8000/";
   const sharedLocalEndpoint = "http://192.168.200.49:8000/";
+  const testEndpoint = "https://api.mes.ferredevs.com/";
 
   return `${localEndpoint}${concat}`;
 }
