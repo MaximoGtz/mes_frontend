898ac83ada7daac6d4ccd12b97d0d48c3cee2a0f
MaximoGtz <maximogtzog@gmail.com>
2025-06-06 10:45:51 -0600

    Navbar responsive actualizado

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,7 +3,46 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import NavbarDrawer from "../layout/NavbarDrawer";
+import { useNavigate } from "react-router-dom";
 export default function MainLayout() {
+  const navigate = useNavigate();
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setDrawerState({ ...drawerState, [anchor]: open });
+  };
+  const handleNavigate = (page) => {
+    switch (page) {
+      case "index":
+        navigate("/");
+        break;
+      case "prod_table":
+        navigate("/tabla");
+        break;
+      case "day_data":
+        navigate("/estatus/dia");
+        break;
+      case "stats":
+        navigate("/estadisticas");
+        break;
+
+      default:
+        navigate("/");
+        break;
+    }
+  };
+  const [drawerState, setDrawerState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
   return (
     <Box
       sx={{
@@ -12,8 +51,13 @@ export default function MainLayout() {
         flexDirection: "column",
       }}
     >
-      <Navbar />
-
+      <Navbar toggleDrawer={toggleDrawer} handleNavigate={handleNavigate} />
+      <NavbarDrawer
+        toggleDrawer={toggleDrawer}
+        state={drawerState}
+        setState={setDrawerState}
+        handleNavigate={handleNavigate}
+      />
       <Box
         sx={{
           flex: 1,
