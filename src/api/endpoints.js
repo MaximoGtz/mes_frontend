27e44fdcea5fcdae27e3e172c0e8517d176a2eb4
Maximo Gutierrez <maximogtzog@gmail.com>
2025-06-11 13:39:40 -0600

    antes del merge de main a develop

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,6 +2,6 @@ export function normalEndpoint(concat) {
   const localEndpoint = "http://127.0.0.1:8000/";
   const sharedLocalEndpoint = "http://192.168.200.49:8000/";
   const testEndpoint = "https://api.mes.ferredevs.com/";
-
+console.log("pequeño cambio");
   return `${localEndpoint}${concat}`;
 }
